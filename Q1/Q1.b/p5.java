8ba16cf39070d7e5cc5c1aec1911d639ab2a2723
amnaansartech-lgtm <amnaansartech@gmail.com>
2026-04-19 23:14:37 +0530

    Update p5.java

--- Q1/Q1.b/p5.java
+++ Q1/Q1.b/p5.java
@@ -1,3 +1,5 @@
+/*5. Write a java program for getting different colors through ArrayList interface and delete nth
+element from the ArrayList object by using remove by index*/
 import java.util.*;
 public class p5 {
 
